114579506647df12906d0528841f7eda6624d689
jramirezmorales <121830872+jramirezmorales@users.noreply.github.com>
2023-01-23 09:20:02 -0600

    second circleci project test

---
+++ tsbb2b-sfg-brewery-master/src/test/java/guru/springframework/brewery/TsbbSfgBreweryApplicationTests.java
@@ -0,0 +1,14 @@
+package guru.springframework.brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+public class TsbbSfgBreweryApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
